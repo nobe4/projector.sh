18fcf27d8495f14bd8e843f20f04ee33928e1a97
nobe4 <nobe4@users.noreply.github.com>
2023-12-03 10:33:34 +0100

    fix(tmux): fix the tmux logic, and refactor the conditions slightly

--- switchers/tmux.sh
+++ switchers/tmux.sh
@@ -1,38 +1,41 @@
 #!/usr/bin/env bash
 #/ Usage: tmux.sh NEW_PROJECT
+#/
+#/ Expect the environment variables:
+#/   PR_STATE_PATH
+#/   PR_BASE_PATH
 
 set -e
-set -x
 
-env | grep PR_
+show_help() { grep ^#/ <"${0}" | cut -c4- ; }
+[ "${*}" == "-h" ] && show_help && exit 0
 
 [ "${1}" == "" ] && exit 0
 
 current_file="${PR_STATE_PATH:?}/current"
-last_file="${PR_STATE_PATH:?}/last"
-last_project="$(<"${last_file}")"
+current_project="$(<"${current_file}")"
+
+echo "${1}" >> "${PR_STATE_PATH:?}/history"
+echo "${1}" > "${current_file}"
 
 # Tmux doesn't like . or : in session names as they represent window index or
 # pane index.
 new_session="${1//[.:]/_}"
-last_session="${last_project//[.:]/_}"
-current_session="$(tmux display-message -p '#S')"
-
-echo "${1}" >> "${PR_STATE_PATH:?}/history"
-current_project="$(<"${current_file}")"
-echo "${1}" > "${current_file}"
 
 if ! tmux has-session -t="${new_session}" 2> /dev/null; then
 	tmux new-session -d -s "${new_session}" -c "${PR_BASE_PATH}/${1}"
 fi
 
-if [ "${TERM_PROGRAM}" == "tmux" ]; then
-	if [ "${current_session}" !=  "${last_session}" ]; then
-		echo "${current_project}" > "${last_file}"
-	fi
-
-	tmux switch-client -t="${new_session}"
-else
+if [ "${TERM_PROGRAM}" != "tmux" ]; then
 	tmux attach-session -t="${new_session}"
+	exit $?
 fi
 
+last_file="${PR_STATE_PATH:?}/last"
+last_project="$(<"${last_file}")"
+
+if [ "${current_project}" !=  "${last_project}" ]; then
+	echo "${current_project}" > "${last_file}"
+fi
+
+tmux switch-client -t="${new_session}"
