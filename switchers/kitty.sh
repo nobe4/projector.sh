a9c70f27f7ed333953395d3b4280ec078ff7c69a
nobe4 <nobe4@users.noreply.github.com>
2024-05-02 12:35:11 +0200

    feat: improve kitty launcher and refactor tmux launcher
    This kitty launcher is used as per https://github.com/nobe4/dotfiles/commit/c90daf9fff4633634f7df492bb337908a60d504f

--- switchers/kitty.sh
+++ switchers/kitty.sh
@@ -4,27 +4,40 @@
 #/ Require the environment variables:
 #/   PR_STATE_PATH
 #/   PR_BASE_PATH
+#/
+#/ Requires kitty to be configured with:
+#/   allow_remote_control yes
 
 set -e
+
+unset FZF_DEFAULT_OPTS PR_SWITCHER
 
 show_help() { grep ^#/ <"${0}" | cut -c4- ; }
 [ "${*}" == "-h" ] && show_help && exit 0
 
 [ "${1}" == "" ] && exit 0
 
+history_file="${PR_STATE_PATH:?}/history"
 current_file="${PR_STATE_PATH:?}/current"
 current_project="$(<"${current_file}")"
-
-echo "${1}" >> "${PR_STATE_PATH:?}/history"
-echo "${1}" > "${current_file}"
-
 last_file="${PR_STATE_PATH:?}/last"
-last_project="$(<"${last_file}")"
+
+echo "${1}" >> "${history_file}"
+echo "${1}" > "${current_file}"
 
 if [ "${current_project}" !=  "${last_project}" ]; then
 	echo "${current_project}" > "${last_file}"
 fi
 
-(
-	kitty --directory "${PR_BASE_PATH}/${1}" --start-as maximized
-) &
+# Focus the os-window by its title OR create a new one
+kitty @ focus-window \
+	--match="title:${1}" \
+	2> /dev/null \
+	||
+kitty @ launch \
+	--type=os-window \
+	--no-response \
+	--title="${1}" \
+	--os-window-title="${1}" \
+	--os-window-state=fullscreen \
+	--cwd="${PR_BASE_PATH}/${1}"
